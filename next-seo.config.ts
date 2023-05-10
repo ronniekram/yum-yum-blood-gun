574c8e175e0f066e0749557f4f052169df87319a
ronniekram <arynn.boniface@gmail.com>
2023-05-09 22:44:09 -0400

    working: capitalize og text

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,26 +1,26 @@
 import type { DefaultSeoProps } from "next-seo";
 
 const config: DefaultSeoProps = {
-  title: `YumYum Bloodgun`,
+  title: `YUMYUM BLOODGUN`,
   description: `A fast-paced, 2-player fighting game that merges high skill combat with roguelike deckbuilder mechanics, in which you build your character as you fight.`,
   canonical: `https://yumyum.cool/`,
   openGraph: {
     type: `website`,
     locale: `en_US`,
     url: `https://yumyum.cool/`,
-    siteName: `YumYum Bloodgun`,
+    siteName: `YUMYUM BLOODGUN`,
     images: [
       {
         url: `https://raw.githubusercontent.com/ronniekram/yum-yum-blood-gun/main/public/opengraph.png`,
         width: 1200,
         height: 630,
-        alt: `YumYum Bloodgun`,
+        alt: `YUMYUM BLOODGUN`,
       },
     ],
   },
   twitter: {
     cardType: `summary_large_image`,
-    site: `YumYum Bloodgun`,
+    site: `YUMYUM BLOODGUN`,
     handle: `@goblindelight`,
   },
 };
