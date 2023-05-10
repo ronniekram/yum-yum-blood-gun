c27e84a847daaf5a363a1739a909343dfc9d7cc1
ronniekram <arynn.boniface@gmail.com>
2023-05-09 22:40:03 -0400

    fix: seo and opengraph image

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,26 +1,26 @@
 import type { DefaultSeoProps } from "next-seo";
 
 const config: DefaultSeoProps = {
-  title: `Yum Yum Blood Gun`,
+  title: `YumYum Bloodgun`,
   description: `A fast-paced, 2-player fighting game that merges high skill combat with roguelike deckbuilder mechanics, in which you build your character as you fight.`,
   canonical: `https://yumyum.cool/`,
   openGraph: {
     type: `website`,
     locale: `en_US`,
     url: `https://yumyum.cool/`,
-    siteName: `Yum Yum Blood Gun`,
+    siteName: `YumYum Bloodgun`,
     images: [
       {
         url: `https://raw.githubusercontent.com/ronniekram/yum-yum-blood-gun/main/public/opengraph.png`,
         width: 1200,
         height: 630,
-        alt: `Yum Yum Blood Gun`,
+        alt: `YumYum Bloodgun`,
       },
     ],
   },
   twitter: {
     cardType: `summary_large_image`,
-    site: `Yum Yum Blood Gun`,
+    site: `YumYum Bloodgun`,
     handle: `@goblindelight`,
   },
 };
